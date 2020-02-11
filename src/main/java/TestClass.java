431e8d19a81eb7ac17d7bddce9844ba4cdae2f67
Mikhail Zotov <38421940+zotovmikhail@users.noreply.github.com>
2020-02-12 00:40:22 +0400

    Implement methods that provide sum and multiplication.

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,11 @@
+public class TestClass {
+
+    public static int sum(int firstArg, int secondArg) {
+        return firstArg + secondArg;
+    }
+
+    public static int multiple(int firstArg, int secondArg) {
+        return firstArg * secondArg;
+    }
+
+}
